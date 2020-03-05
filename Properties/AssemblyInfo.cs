8652975848adb114bd23ef0a231520e435ce1d51
Data Juggler <corby@datajuggler.com>
2020-03-04 23:22:28 -0600

    Checking in the .Net Framework version.

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("DataJuggler.PixelDatabase.Net")]
+[assembly: AssemblyDescription("PixelDatabase is a List of PixelInformation objects, that you can use LINQ expressions or Bitmap Query Language expressions to update images.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Data Juggler -")]
+[assembly: AssemblyProduct("PixelDatabase")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("3cd054eb-8fa9-4bec-bebb-b5302c1020ea")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.3.0")]
+[assembly: AssemblyFileVersion("1.0.3.0")]
